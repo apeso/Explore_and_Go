d96f1bb11f7fe7d621b87b7bcb34a5ed91182fe2
kkelam <kkelam01@fesb.hr>
2022-01-17 18:16:21 +0100

    novo

--- app/src/main/java/com/example/projekt/LoginActivity.java
+++ app/src/main/java/com/example/projekt/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.projekt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -84,19 +85,21 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        //sign in-->provjeravamo postoji li u bazi
+        //sign in-->provjeravamo postoji li
         mAuth.signInWithEmailAndPassword(email, pass)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful())
                         {
+                            Log.d("KATE", "login uspjesan");
                             //redirect to home
                             Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(homeIntent);
                         }
                         else
                         {
+                            Log.d("KATE", "login neuspjesan");
                             Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
                         }
                     }
--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -2,79 +2,55 @@ package com.example.projekt;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.gson.Gson;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvIme;
-
-    public Country[] countries;
-    final Gson mGson = new Gson();
-    Button btn;
-
-    FirebaseFirestore fStore;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //tvIme = (TextView) findViewById(R.id.ime);
-        fStore = FirebaseFirestore.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = user.getUid();
-        String name = user.getDisplayName();
-        tvIme.setText(uid);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomnavigationbar);
+        bottomNavigationView.setBackground(null);
 
-        //btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                /*String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "countries.json");
-                countries = mGson.fromJson(jsonFileString, Country[].class);
-                for(int i = 0; i < countries.length; i++)
-                {
-                    Log.i("data", ">Item" + i + "\n" + countries[i]);
-                    Toast.makeText(getApplicationContext(),"data"+countries[i],Toast.LENGTH_LONG).show();
-                }*/
-                // Write a message to the database
-                fStore.collection("users")
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        Log.d("KATE", document.getId() + " => " + document.getData());
-                                    }
-                                } else {
-                                    Log.w("KATE", "Error getting documents.", task.getException());
-                                }
-                            }
-                        });
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.uno:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        return true;
+                    case R.id.dos:
+                        startActivity(new Intent(getApplicationContext(), NewTripActivity.class));
+                        return true;
+                    case R.id.tres:
+                        startActivity(new Intent(getApplicationContext(), MyTravelsActivity.class));
+                        return true;
+                    case R.id.cuatro:
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        return true;
+                }
+                return true;
             }
         });
     }
